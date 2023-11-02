196b5404628b26af8fc9c833bd36483363f5f0ab
Anjeli-Jac <98351136+Anjeli-Jac@users.noreply.github.com>
2023-11-02 15:43:24 -0400

    Notes (#11)
    * added list display, single note display, view list in notes create
    
    todo: create edit notes screen --> look like the create front but already has text
    
    * tmp save
    
    * added alertDialogs and user validation
    
    * CRUD fully implemented
    
    * single white space before start of function body in NotesListScreen
    
    * single white space before start of function body in SingleNoteLayout and SingleNoteElementDisplay

---
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/noteInput/DisplayList.kt
@@ -0,0 +1,156 @@
+package com.example.habittrackerapp.noteInput
+
+import Routes
+import coil.compose.AsyncImage
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.Alignment
+import com.example.habittrackerapp.R
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import com.example.habittrackerapp.LocalNavController
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
+/**
+ * Displays all the elements of the StringBuilder list in a card container*/
+@Composable
+fun DisplayNotesList(notesList: SnapshotStateList<Note>)
+{
+    val navController= LocalNavController.current
+    var openDialog by rememberSaveable { mutableStateOf(true)}
+    var clicked by rememberSaveable { mutableStateOf(false) };
+    LazyColumn{
+        itemsIndexed(notesList) { index, note ->
+            Card(  modifier = Modifier
+                .padding(16.dp),
+                shape = RoundedCornerShape(7.dp))
+            {
+
+                    Row(modifier = Modifier.padding(10.dp)) {
+
+                        //makes the image into an icon
+                        AsyncImage(
+                            model = note.urlImage,
+                            error= painterResource(id = R.drawable.no_image_placeholder),
+                            modifier = Modifier
+                                .size(5.dp)
+                                .padding(5.dp)
+                                .clip(MaterialTheme.shapes.small),
+                            contentScale = ContentScale.Crop,
+                            contentDescription = null
+                        )
+                        Column(modifier = Modifier
+                            .clickable {
+                                navController.navigate("SingleNoteScreenRoute/${index}")
+                            }
+                            .fillMaxWidth()
+                            .padding(10.dp))
+                        {
+                            Text(
+                                text = note.title,
+                                fontFamily = FontFamily.Monospace
+                            )
+                            Text(
+                                text = note.description, fontFamily = FontFamily.Monospace
+                            )
+                            Row(modifier = Modifier
+                                .align(Alignment.End))
+                            {
+
+                                Button(onClick = { navController.navigate("EditNoteScreenRoute/${index}")},
+                                    modifier = Modifier
+                                        .padding(0.dp)){
+                                    Text(text = "edit")
+                                }
+                                Button(onClick = {clicked=true},
+                                    modifier = Modifier
+                                        .padding(10.dp,0.dp))
+                                {
+                                    Text(text = "remove")
+                                }
+                                if(clicked){
+                                    if(openDialog){
+                                        AlertDialog(
+                                            onDismissRequest = {  openDialog=false },
+                                            confirmButton = {
+                                                TextButton(
+                                                    onClick = {
+                                                        openDialog=false
+                                                        notesList.remove(note)
+                                                        if(notesList.isEmpty()){
+                                                            navController.navigate(Routes.Note.route)
+                                                        }
+
+                                                    }
+                                                ) {Text("confirm")}
+                                            },
+                                            title={
+                                                Text("Delete")
+                                            },
+                                            text = {
+                                                Text(text = "Are you sure?")
+                                            }
+                                        )
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+
+            }
+
+            Divider(modifier = Modifier.fillMaxWidth())//Separates each item
+        }
+    }
+
+}
+
+
+//Taken from slide 30 AppDev2_Day_11-12-13_final
+@Composable
+fun <T: Any> rememberMutableStateListOf(vararg elements: T): SnapshotStateList<T> {
+    return rememberSaveable(
+        saver = listSaver(
+            save = { stateList ->
+                if (stateList.isNotEmpty()) {
+                    val first = stateList.first()
+                    if (!canBeSaved(first)) {
+                        throw IllegalStateException("${first::class} cannot be saved. By default only types which can be stored in the Bundle class can be saved.")
+                    }
+                }
+                stateList.toList()
+            },
+            restore = { it.toMutableStateList() }
+        )
+    ) {
+        elements.toList().toMutableStateList()
+    }
+}
+
---
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/noteInput/EditNote.kt
@@ -0,0 +1,139 @@
+package com.example.habittrackerapp.noteInput
+
+import android.widget.Toast
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
+import com.example.habittrackerapp.LocalNavController
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextButton
+
+/**
+ * It contains text-fields for user input and saves it to a list
+ * */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNote(note: Note) {
+
+    var title by rememberSaveable { mutableStateOf(note.title) }
+    val maxLength = 20
+    var urlImage by rememberSaveable { mutableStateOf(note.urlImage!!) }
+    var context = LocalContext.current;
+    var navController= LocalNavController.current
+    var description by rememberSaveable { mutableStateOf(note.description) }
+    val scrollState = rememberScrollState()
+    var openDialog by rememberSaveable { mutableStateOf(true)}
+    var clicked by rememberSaveable { mutableStateOf(false) };
+
+    Column(modifier = Modifier.padding(2.dp))
+    {
+        //title text field
+        TextField(
+            value = title,
+            onValueChange = {
+                if (it.length <= maxLength) title = it
+                else Toast.makeText(
+                    context,
+                    "Cannot be more than 20 Characters",
+                    Toast.LENGTH_SHORT
+                ).show()
+            },
+            placeholder = { Text(note.title) },
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .padding(20.dp, 0.dp)
+                .fillMaxWidth()
+        )
+        //body/content description text field
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            modifier = Modifier
+                .height(250.dp)
+                .padding(20.dp, 0.dp)
+                .verticalScroll(scrollState),
+            placeholder = { Text(note.description) },
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+        )
+
+        OutlinedTextField(value = urlImage, onValueChange = { urlImage = it },
+            modifier = Modifier
+                .padding(10.dp, 40.dp, 10.dp, 10.dp)
+                .fillMaxWidth()
+                .verticalScroll(scrollState),
+            singleLine = true)
+
+
+
+        //Buttons
+        Row(modifier = Modifier
+            .padding(0.dp, 0.dp, 20.dp, 0.dp)
+            .align(Alignment.End))
+        {
+            //saves input to the list
+            Button(
+                onClick =
+                {   clicked=true;
+                    openDialog=true;
+                }, modifier = Modifier.padding(0.dp))
+            { Text("save") }
+            //goes back to list screen
+            Button(onClick = {
+                navController.navigate(Routes.ViewList.route)
+            }, modifier = Modifier.padding(10.dp, 0.dp)) { Text("cancel") }
+
+            if(clicked){
+                if(openDialog){
+                    AlertDialog(
+                        onDismissRequest = {  openDialog=false },
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    openDialog=false
+                                    note.title = title;
+                                    note.description = description;
+                                    note.urlImage = urlImage;
+                                    navController.navigate(Routes.ViewList.route)
+                                }
+                            ) {Text("confirm")}
+                        },
+                        title={
+                            Text("Edit")
+                        },
+                        text = {
+                            Text(text = "Save the modified note of the title\n$title")
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
