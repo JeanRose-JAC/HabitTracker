d82921d7fc11241b01b020d65349064f81938243
Cindy-Coulibaly <98350788+Cindy-Coulibaly@users.noreply.github.com>
2023-11-16 15:39:40 -0500

    the launcher is done!

--- HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/navigation/DeepScreen.kt
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/navigation/DeepScreen.kt
@@ -6,8 +6,11 @@ import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,8 +19,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import com.example.habittrackerapp.layout.MainLayout
 
@@ -28,13 +34,14 @@ fun DeepScreen(id: String?) {
     val activity = localContext as ComponentActivity
     var input by rememberSaveable { mutableStateOf("") }
 
-    Column {
-        Text("Welcome $id")
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Welcome $id", modifier=Modifier.padding(20.dp))
         TextField(
             value = input,
             onValueChange = {input=it},
             label={ Text("Please enter to change your name") },
-            isError=input.isEmpty()
+            isError=input.isEmpty(),
+            modifier=Modifier.padding(20.dp)
         )
 
         Button(onClick = {
@@ -42,7 +49,7 @@ fun DeepScreen(id: String?) {
             resultIntent.putExtra("resultData", input) // Set the value to return as a result
             localContext.setResult(Activity.RESULT_OK, resultIntent)
             localContext.finish() // Finish the activity
-        }) {
+        }, colors = ButtonDefaults.buttonColors(containerColor = Color(148,156,241)), modifier=Modifier.padding(20.dp)) {
             Text("Send back a value to launching app")
         }
     }
