aa9c68c45dff146ba3608559cf35444bdcfcb83d
Anjeli-Jac <98351136+Anjeli-Jac@users.noreply.github.com>
2023-11-27 15:14:43 -0500

    added images and fixed the user sign up page

--- HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/signInSignUp/SignInSignUpScreen.kt
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/signInSignUp/SignInSignUpScreen.kt
@@ -1,17 +1,37 @@
 package com.example.habittrackerapp.signInSignUp
 
 import Routes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.habittrackerapp.LocalNavController
+import com.example.habittrackerapp.R
 import com.example.habittrackerapp.auth.AuthViewModel
 import com.example.habittrackerapp.auth.AuthViewModelFactory
 import com.example.habittrackerapp.data
@@ -30,18 +50,18 @@ fun SignSignUpScreen(savedUserViewModel: SavedUserViewModel = viewModel(factory 
     val activeUser = MyViewModel.activeUser.collectAsState()
     var onceSigned by rememberSaveable { mutableStateOf(true) }
 
-    Column {
+
         if( !myUiState.email.isEmpty()) {
             if (myUiState.email != "empty") {
-                if(onceSigned){
-                    authViewModel.signIn(myUiState.email,myUiState.password)
+                if (onceSigned) {
+                    authViewModel.signIn(myUiState.email, myUiState.password)
                     MyViewModel.getUser(myUiState.email)
                     onceSigned = false
                 }
 
-                if(activeUser.value.FirstName.isNotEmpty()){
+                if (activeUser.value.FirstName.isNotEmpty()) {
                     println("in signIn-> ${activeUser.value.FirstName}")
-                    userInput.FirstName=activeUser.value.FirstName
+                    userInput.FirstName = activeUser.value.FirstName
                     userInput.Email = activeUser.value.Email
                     userInput.Gender = activeUser.value.Gender
                     userInput.LastName = activeUser.value.LastName
@@ -49,19 +69,64 @@ fun SignSignUpScreen(savedUserViewModel: SavedUserViewModel = viewModel(factory 
                     userInput.Password = activeUser.value.Password
                     navController.navigate(Routes.Setting.route)
                 }
-            }
-            else{
-                Text("Please")
-                Button(onClick = {navController.navigate(Routes.SignUp.route)}) {
-                    Text("Sign Up")
-                }
-                Text("or")
+            } else {
+                val scrollState = rememberScrollState()
+                Image(
+                    painter = painterResource(R.drawable.splashclock),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds,
+                )
+                Column(
+                    modifier = Modifier.verticalScroll(scrollState),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-                Button(onClick = {navController.navigate(Routes.SignIn.route)}) {
-                    Text("Sign In")
+
+                    Card(Modifier.padding(20.dp, 190.dp)) {
+
+                    }
+                    Text(
+                        "Habit Minder",
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        "Track Today. Transform Tomorrow.\nYour Habits. Your Success!",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 10.dp, 0.dp, 8.dp),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.W500,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Button(
+                        onClick = { navController.navigate(Routes.SignUp.route) },
+                        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp),
+                    ) {
+                        Text("GET STARTED")
+                    }
+
+                    Row(modifier = Modifier.align(CenterHorizontally)) {
+                        Text(
+                            "Already have an account? ",
+                            modifier = Modifier.padding(14.dp, 14.dp, 0.dp, 14.dp)
+                        )
+                        TextButton(onClick = { navController.navigate(Routes.SignIn.route) },) {
+                            Text("Login")
+
+
+                        }
+                    }
                 }
             }
-        }
+
+
     }
 
 }
