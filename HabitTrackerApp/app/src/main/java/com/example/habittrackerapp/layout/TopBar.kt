a9bfb227bf883cd95c9cd0afb44e4e04e46d0ae3
Anjeli-Jac <98351136+Anjeli-Jac@users.noreply.github.com>
2023-11-27 14:58:37 -0500

    tmpsave

--- HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/layout/TopBar.kt
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/layout/TopBar.kt
@@ -7,9 +7,16 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberAsyncImagePainter
 import com.example.habittrackerapp.LocalNavController
+import com.example.habittrackerapp.R
+
 /**
  * Composable for the shared top bar.
  * There is a back button that brings the user to the previous page.
@@ -19,6 +26,12 @@ import com.example.habittrackerapp.LocalNavController
 fun TopBarScreen(title:String) {
     val navController = LocalNavController.current
     CenterAlignedTopAppBar(
+        colors= TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        ),
         title={Text(title)},
         navigationIcon = {
             IconButton(onClick = {navController.navigateUp()}){
@@ -27,7 +40,18 @@ fun TopBarScreen(title:String) {
                     contentDescription = "Go Back"
                 )
             }
-        },
+
+
+
+        }, actions = {
+            IconButton(onClick = { navController.navigate(Routes.About.route)}) {
+                Icon(painter = rememberAsyncImagePainter(R.drawable.logo3, contentScale = ContentScale.FillWidth),
+                    contentDescription = "Logo",
+                    tint = Color.Unspecified
+                )
+            }
+        }
+
     )
 
 }
