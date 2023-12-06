ad823f6ad9195c9e6c2bc57f46cb25431b8543a2
Anjeli-Jac <98351136+Anjeli-Jac@users.noreply.github.com>
2023-12-06 00:32:42 -0500

    tmp save

--- HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/MainActivity.kt
+++ HabitTrackerApp/app/src/main/java/com/example/habittrackerapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.habittrackerapp
 
 import Router
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.habittrackerapp.layout.MainLayout
@@ -21,6 +25,7 @@ import com.example.habittrackerapp.signInSignUp.rememberMutableStateListOf
 import com.example.habittrackerapp.model.noteViewModel.Note
 import com.example.habittrackerapp.ui.theme.HabitTrackerAppTheme
 import com.example.habittrackerapp.model.userViewModel.User
+import com.example.habittrackerapp.splash.SplashViewModel
 import com.google.firebase.FirebaseApp
 
 val LocalNavController = compositionLocalOf<NavController> { error("No NavController found!") }
@@ -31,8 +36,13 @@ val LocalNotesList = compositionLocalOf<SnapshotStateList<Note>> { error("No not
 //Source: https://developer.android.com/codelabs/basic-android-kotlin-compose-persisting-data-room#0
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen =installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
         FirebaseApp.initializeApp(this)
         setContent {
             
